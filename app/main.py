f4d7b5a694c24374068ff4d4c37d15d2d2def046
thergh <lukasz.ciswicki@gmail.com>
2025-09-14 01:01:00 +0200

    db connection

--- app/main.py
+++ app/main.py
@@ -1,8 +1,31 @@
+from . import models, schemas, utils
+from .database import engine, get_db
 from fastapi import Body, FastAPI, Response, status, HTTPException, Depends
 from typing import Optional, List
+import psycopg2
+from psycopg2.extras import RealDictCursor
+import time
+
 
 
 # uvicorn app.main:app --reload
 
 app = FastAPI()
+
+models.Base.metadata.create_all(bind=engine)
+
+
+while True:
+	try:
+		conn = psycopg2.connect(
+			host='localhost', database='investment', user='postgres',
+			password='password', cursor_factory=RealDictCursor
+		)
+		cursor = conn.cursor()
+		print("Databse connection successful")
+		break
+	except Exception as error:
+		print("Connection unsuccessful")
+		time.sleep(2)
+		
 
---
+++ app/models.py
@@ -0,0 +1,22 @@
+from .database import Base
+from sqlalchemy import Column, Integer, String, Boolean, Float
+from sqlalchemy.sql.sqltypes import TIMESTAMP
+from sqlalchemy.sql.expression import text
+
+
+class Investment(Base):
+    __tablename__ = "investments"
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    title = Column(String, nullable=False)
+    amount = Column(Float, nullable=False)
+    bought_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
+    sold_at = Column(TIMESTAMP(timezone=True), nullable=True, server_default=text('null'))
+
+
+class User(Base):
+    __tablename__="users"
+    id = Column(Integer, primary_key=True, nullable=False)
+    email = Column(String, nullable=False, unique=True)
+    password = Column(String, nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
