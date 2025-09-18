cdd62b3475669a270634f27b82b69c6918b24541
thergh <lukasz.ciswicki@gmail.com>
2025-09-18 23:36:58 +0200

    token verification

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -15,11 +15,11 @@ def login(user_credentials: OAuth2PasswordRequestForm = Depends(), db:Session = 
 
     # user of this email doesn't exist
     if not user:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Invalid Credentials")
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=f"Invalid Credentials")
     
     # wrong password
     if not utils.verify_password(user_credentials.password, user.password):
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Invalid Credentials")
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=f"Invalid Credentials")
     
     access_token = oauth2.create_access_token(data = {"user_id": user.id})
 
