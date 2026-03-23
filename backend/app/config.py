ab1821f314d40a82d5eab7799037d198704670cd
thergh <lukasz.ciswicki@gmail.com>
2026-03-23 13:02:57 +0100

    fix: comma delimitation for cors env var

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,4 +1,6 @@
 from pydantic_settings import BaseSettings
+from pydantic import field_validator
+import json
 
 class Settings(BaseSettings):
 	database_hostname: str
@@ -10,6 +12,15 @@ class Settings(BaseSettings):
 	algorithm: str
 	access_token_expire_minutes: int
 	origins: list[str] = ["http://localhost:5173"]
+
+	@field_validator("origins", mode="before")
+	@classmethod
+	def assemble_cors_origins(cls, v: str | list[str]) -> list[str]:
+		if isinstance(v, str) and not v.startswith("["):
+			return [i.strip() for i in v.split(",")]
+		elif isinstance(v, str) and v.startswith("["):
+			return json.loads(v)
+		return v
 
 	class Config:
 		env_file = ".env"
